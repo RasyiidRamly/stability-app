f2b04756191a3ee8707eb31916a81d7575544fdf
zurgigas <rasyiid.ramly@gmail.com>
2026-04-26 20:58:21 +0700

    Build, Push, Deploy, Rollout and healthcheck

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('OK v3'));
-app.get('/health', (req, res) => res.send('healthy'));
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'ok',
+    uptime: process.uptime()
+  });
+});
 
 app.listen(3000, () => console.log('running on 3000'));
 
